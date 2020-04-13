484d123a22a13856b90acda462b641854d34d1d9
arogss <abcarogundade@yahoo.co.uk>
2020-04-13 16:36:51 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -28,14 +28,14 @@ app.use("/api/workouts", workouts)
 app.use("/api/clients", clients)
 app.use("/api/auth", auth)
 
-// //server static assests
-// if(process.env.NODE_ENV === 'production'){
-//     //set static folder
-//     app.use(express.static('client/build'));
+//server static assests
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
 
-//     app.get('*', (req, res) =>{
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
